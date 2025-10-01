21fedd7834297b51f5dda50fcf3a5417db5d3e9e
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-01 20:25:04 -0300

    Crea Base de datos

---
+++ app/src/main/java/edu/ucne/erick_estrada_ap2_p1/data/database/HuacalDb.kt
@@ -0,0 +1,18 @@
+package edu.ucne.erick_estrada_ap2_p1.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.erick_estrada_ap2_p1.data.huacal.HuacalDao
+import edu.ucne.erick_estrada_ap2_p1.data.huacal.HuacalEntity
+
+@Database(
+    entities = [
+        HuacalEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+
+abstract class HuacalDb: RoomDatabase(){
+    abstract fun HuacalDao(): HuacalDao
+}
