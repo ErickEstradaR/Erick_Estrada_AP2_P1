14331d47664a54452f44157c0c783c176c5833aa
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-01 20:07:03 -0300

    Crea Modelo Huacal

---
+++ app/src/main/java/edu/ucne/erick_estrada_ap2_p1/domain/model/Huacal.kt
@@ -0,0 +1,10 @@
+package edu.ucne.erick_estrada_ap2_p1.domain.model
+
+data class Huacal(
+
+    val IdEntrada: Int? = null,
+    val Fecha : String = "",
+    val NombreCliente: String = "",
+    val Cantidad: Int = 0,
+    val Precio: Float
+)
