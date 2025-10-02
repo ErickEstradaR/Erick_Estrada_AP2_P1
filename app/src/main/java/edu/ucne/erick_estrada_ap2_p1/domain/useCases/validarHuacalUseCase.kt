57d1052615320c2e4759a0a8cfdd4bb56cfbf59d
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-01 22:03:27 -0300

    Mejora validaciones de huacales

--- app/src/main/java/edu/ucne/erick_estrada_ap2_p1/domain/useCases/validarHuacalUseCase.kt
+++ app/src/main/java/edu/ucne/erick_estrada_ap2_p1/domain/useCases/validarHuacalUseCase.kt
@@ -8,8 +8,16 @@ class validarHuacalUseCase(
 ) {
     suspend operator fun invoke(huacal: Huacal): Result<Unit> {
 
-        if (huacal.nombreCliente.isBlank() || huacal.cantidad <= 0 || huacal.precio <= 0) {
+        if (huacal.nombreCliente.isBlank() ) {
             return Result.failure(Exception("Nombre esta vacio"))
+        }
+        if (huacal.cantidad <= 0)
+        {
+            return Result.failure(Exception("La cantidad es incorrecta"))
+        }
+        if (huacal.precio <= 0)
+        {
+            return Result.failure(Exception("El Precio es invalido"))
         }
 
         return Result.success(Unit)
