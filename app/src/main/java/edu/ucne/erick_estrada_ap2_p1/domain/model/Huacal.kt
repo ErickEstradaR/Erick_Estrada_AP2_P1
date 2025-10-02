0d44a66c9057628ef0b197038d2c45db57e29649
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-01 23:47:11 -0300

    Modifica fecha

--- app/src/main/java/edu/ucne/erick_estrada_ap2_p1/domain/model/Huacal.kt
+++ app/src/main/java/edu/ucne/erick_estrada_ap2_p1/domain/model/Huacal.kt
@@ -3,7 +3,7 @@ package edu.ucne.erick_estrada_ap2_p1.domain.model
 data class Huacal(
 
     val idEntrada: Int? = null,
-    val fecha : String = "",
+    val fecha : String? = "",
     val nombreCliente: String = "",
     val cantidad: Int = 0,
     val precio: Float
