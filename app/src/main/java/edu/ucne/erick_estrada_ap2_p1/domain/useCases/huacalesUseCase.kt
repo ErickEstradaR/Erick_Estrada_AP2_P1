235fce694c34f862b15a957c9d0fff36c88bec44
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-01 21:34:23 -0300

    agrupa casos de uso de huacal

---
+++ app/src/main/java/edu/ucne/erick_estrada_ap2_p1/domain/useCases/huacalesUseCase.kt
@@ -0,0 +1,10 @@
+package edu.ucne.erick_estrada_ap2_p1.domain.useCases
+
+class huacalesUseCase (
+    val eliminarHuacal : eliminarHuacalUseCase,
+    val guardarHuacal : guardarHuacalUseCase,
+    val obtenerHuacal :  obtenerHuacalUseCase,
+    val obtenerHuacales: obtenerHuacalesUseCase,
+    val validarHuacal: validarHuacalUseCase
+
+)
